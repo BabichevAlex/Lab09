1805d67963fca1e2477c370af2929a0c9b0bff40
SashaBabichev <35573361+Kochka1@users.noreply.github.com>
2019-05-25 14:47:05 +0200

    Add files via upload

---
+++ src/lab9FileSameDoubleSearcher/FileSameDoubleSearcher.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9FileSameDoubleSearcher;
+/**
+ *
+ * @author Andrey
+ */
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSameDoubleSearcher {
+
+    private double accuracy = 0.001;
+    private String fileReadPath;
+    private String fileWritePath;
+    private int countOfSymbInLine;
+
+    public FileSameDoubleSearcher(String fileReadPath, String fileWritePath, int countOfSymbInLine, double accuracy) throws Exception {
+        setAccuracy(accuracy);
+        this.fileReadPath = fileReadPath;
+        this.fileWritePath = fileWritePath;
+        setCountOfSymbInLine(countOfSymbInLine);
+    }
+
+    public FileSameDoubleSearcher(String fileReadPath, String fileWritePath, int countOfSymbInLine) throws Exception {
+        this.fileReadPath = fileReadPath;
+        this.fileWritePath = fileWritePath;
+        setCountOfSymbInLine(countOfSymbInLine);
+    }
+
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(double accuracy) throws Exception {
+        if (accuracy < 0)
+            throw new Exception("count of symbols in line is negative");
+        if (accuracy == 0)
+            throw new Exception("count of symbols in line is zero");
+        this.accuracy = accuracy;
+    }
+
+    public String getFileReadPath() {
+        return fileReadPath;
+    }
+
+    public void setFileReadPath(String fileReadPath) {
+        this.fileReadPath = fileReadPath;
+    }
+
+    public String getFileWritePath() {
+        return fileWritePath;
+    }
+
+    public void setFileWritePath(String fileWritePath) {
+        this.fileWritePath = fileWritePath;
+    }
+
+    public int getCountOfSymbInLine() {
+        return countOfSymbInLine;
+    }
+
+    public void setCountOfSymbInLine(int countOfSymbInLine) throws Exception {
+        if (countOfSymbInLine < 0)
+            throw new Exception("count of symbols in line is negative");
+        if (countOfSymbInLine == 0)
+            throw new Exception("count of symbols in line is zero");
+        this.countOfSymbInLine = countOfSymbInLine;
+    }
+
+    public void search() throws Exception {
+        File file = new File(fileWritePath);
+        if(!file.exists()) {
+            file.createNewFile();
+        }
+        try(RandomAccessFile fileReader = new RandomAccessFile(fileReadPath, "r");
+            FileWriter writer = new FileWriter(fileWritePath, false)) {
+
+            countOfSymbInLine += 2;
+            String lineSeparator = System.getProperty("line.separator");
+            long countOfLine = fileReader.length() / countOfSymbInLine;
+            Double ai, aj;
+            List<Double> exists = new ArrayList<>();
+            writer.write("Results:" + lineSeparator);
+
+            for (long i = 0; i < countOfLine; i++) {
+                fileReader.seek(i * countOfSymbInLine);
+                ai = Double.parseDouble(fileReader.readLine());
+                if(exists.contains(ai))
+                    continue;
+                while(fileReader.getFilePointer() < fileReader.length()) {
+                    aj = Double.parseDouble(fileReader.readLine());
+                    if (Math.abs(Math.abs(ai) - Math.abs(aj)) < accuracy) {
+                        writer.write(ai.toString() + " = " + aj.toString() + lineSeparator);
+                        exists.add(Math.abs(ai));
+                        break;
+                    }
+                }
+            }
+
+            writer.flush();
+        }
+        catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FileSameDoubleSearcher)) return false;
+
+        FileSameDoubleSearcher that = (FileSameDoubleSearcher) o;
+
+        if (Double.compare(that.getAccuracy(), getAccuracy()) != 0) return false;
+        if (getCountOfSymbInLine() != that.getCountOfSymbInLine()) return false;
+        if (!getFileReadPath().equals(that.getFileReadPath())) return false;
+        return getFileWritePath().equals(that.getFileWritePath());
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(getAccuracy());
+        result = (int) (temp ^ (temp >>> 32));
+        result = 31 * result + getFileReadPath().hashCode();
+        result = 31 * result + getFileWritePath().hashCode();
+        result = 31 * result + getCountOfSymbInLine();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "FileSameDoubleSearcher{" +
+                "accuracy=" + accuracy +
+                ", fileReadPath='" + fileReadPath + '\'' +
+                ", fileWritePath='" + fileWritePath + '\'' +
+                ", countOfSymbInLine=" + countOfSymbInLine +
+                '}';
+    }
+}
---
+++ test/lab9SameDoubleSearcher/Lab8PalindromTest.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9SameDoubleSearcher;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSameDoubleSearcherTest {
+
+    final String resultPath = "target\\test-classes\\ua\\edu\\chmnu\\fks\\oop\\Lab09\\result.txt";
+    FileSameDoubleSearcher fileSameDoubleSearcher;
+
+    {
+        try {
+            fileSameDoubleSearcher = new FileSameDoubleSearcher("src\\test\\java\\ua\\edu\\chmnu\\fks\\oop\\Lab09\\numbers.txt", resultPath, 8);
+        }
+        catch (Exception ex) {}
+    }
+
+    @Test
+    public void constructorTest() throws Exception {
+        FileSameDoubleSearcher fileSameDoubleSearcher = new FileSameDoubleSearcher("E:\\Работы\\Программирование\\University\\Java University\\a.txt", "E:\\Работы\\Программирование\\University\\Java University\\result.txt", 8, 0.001);
+    }
+
+    @Test
+    void searchTest() throws Exception {
+        assertDoesNotThrow(() -> fileSameDoubleSearcher.search());
+        try(BufferedReader reader = new BufferedReader(new FileReader(resultPath))) {
+            assertEquals("Results:", reader.readLine());
+            assertEquals("2212.34 = 2212.34", reader.readLine());
+            assertEquals("24.7234 = -24.7234", reader.readLine());
+            assertEquals("34.1834 = 34.1834", reader.readLine());
+        }
+        catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    @Test
+    void fileWritePathSetterGetterTest() throws Exception {
+        fileSameDoubleSearcher.setFileWritePath("asd.txt");
+        assertEquals("asd.txt", fileSameDoubleSearcher.getFileWritePath());
+        fileSameDoubleSearcher.setFileWritePath("a.txt");
+        assertEquals("a.txt", fileSameDoubleSearcher.getFileWritePath());
+        fileSameDoubleSearcher.setFileWritePath("fff.txt");
+        assertEquals("fff.txt", fileSameDoubleSearcher.getFileWritePath());
+        fileSameDoubleSearcher.setFileWritePath("double.txt");
+        assertEquals("double.txt", fileSameDoubleSearcher.getFileWritePath());
+    }
+
+    @Test
+    void fileReadPathSetterGetterTest() throws Exception {
+        fileSameDoubleSearcher.setFileReadPath("asd.txt");
+        assertEquals("asd.txt", fileSameDoubleSearcher.getFileReadPath());
+        fileSameDoubleSearcher.setFileReadPath("a.txt");
+        assertEquals("a.txt", fileSameDoubleSearcher.getFileReadPath());
+        fileSameDoubleSearcher.setFileReadPath("fff.txt");
+        assertEquals("fff.txt", fileSameDoubleSearcher.getFileReadPath());
+        fileSameDoubleSearcher.setFileReadPath("double.txt");
+        assertEquals("double.txt", fileSameDoubleSearcher.getFileReadPath());
+    }
+
+    @Test
+    void accuracySetterGetterTest() throws Exception {
+        fileSameDoubleSearcher.setAccuracy(2.3);
+        assertEquals(2.3, fileSameDoubleSearcher.getAccuracy());
+        fileSameDoubleSearcher.setAccuracy(3);
+        assertEquals(3, fileSameDoubleSearcher.getAccuracy());
+        fileSameDoubleSearcher.setAccuracy(1.5);
+        assertEquals(1.5, fileSameDoubleSearcher.getAccuracy());
+        fileSameDoubleSearcher.setAccuracy(1);
+        assertEquals(1, fileSameDoubleSearcher.getAccuracy());
+
+        assertThrows(Exception.class, () -> fileSameDoubleSearcher.setAccuracy(-3.8));
+        assertThrows(Exception.class, () -> fileSameDoubleSearcher.setAccuracy(0));
+    }
+
+    @Test
+    void countOfSymbInLineSetterGetterTest() throws Exception {
+        fileSameDoubleSearcher.setCountOfSymbInLine(2);
+        assertEquals(2, fileSameDoubleSearcher.getCountOfSymbInLine());
+        fileSameDoubleSearcher.setCountOfSymbInLine(9);
+        assertEquals(9, fileSameDoubleSearcher.getCountOfSymbInLine());
+        fileSameDoubleSearcher.setCountOfSymbInLine(3);
+        assertEquals(3, fileSameDoubleSearcher.getCountOfSymbInLine());
+        fileSameDoubleSearcher.setCountOfSymbInLine(4);
+        assertEquals(4, fileSameDoubleSearcher.getCountOfSymbInLine());
+
+        assertThrows(Exception.class, () -> fileSameDoubleSearcher.setCountOfSymbInLine(-3));
+        assertThrows(Exception.class, () -> fileSameDoubleSearcher.setCountOfSymbInLine(0));
+    }
+}
